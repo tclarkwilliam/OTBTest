496fea17bda06dad56be61b6ba343a65ad5886c6
Tom Clark <tclarkwilliam@gmail.com>
2017-02-21 20:46:10 +0000

    Added hotel model tests

---
+++ OTBTestTests/JSON/JSONLoader.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+@testable import OTBTest
+
+struct JSONLoader {
+
+  static func load<T>(_ fileName: String, forClass: T.Type) -> JSONDictionary? {
+    // swiftlint:disable force_cast
+    let testBundle = Bundle(for: forClass as! AnyClass)
+    guard let path = testBundle.path(forResource: fileName, ofType: "json"),
+          let data = try? Data(contentsOf: URL(fileURLWithPath: path)),
+          let json = try? JSONSerialization.jsonObject(with: data, options: []) else { return nil }
+    return json as? JSONDictionary
+  }
+
+}
---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, "10.0"
+use_frameworks!
+
+target 'OTBTestTests' do
+  pod 'Quick', '~> 1.0.0'
+  pod 'Nimble', '~> 5.0.0'
+end
